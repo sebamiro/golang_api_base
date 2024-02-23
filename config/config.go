9d426d3d052fb30cb8a6fde278c2780bbfca44e0
sebamiro <seba.mi.21@gmail.com>
2024-02-23 19:46:01 +0100

    Added Config module

---
+++ config/config.go
@@ -0,0 +1,86 @@
+package config
+
+import (
+	"log"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+const (
+	TemplateExt  = "html"
+	StaticDir    = "static"
+	StaticPrefix = "files"
+)
+
+type (
+	Config struct {
+		HTTP HTTPConfig
+		App  AppConfig
+		Data DatabaseConfig
+	}
+
+	HTTPConfig struct {
+		Hostname     string
+		Port         uint16
+		ReadTimeout  time.Duration
+		WrtieTimeout time.Duration
+		IdleTimeout  time.Duration
+		TLS          struct {
+			Enabled     bool
+			Certificate string
+			Key         string
+		}
+	}
+
+	AppConfig struct {
+		Name          string
+		EncryptionKey string
+		Timeout       time.Duration
+		PasswordToken struct {
+			Expiration time.Duration
+			Lenght     int
+		}
+		EmailVerificationTokenExpiration time.Duration
+	}
+
+	DatabaseConfig struct {
+		Hostname     string
+		Port         uint16
+		User         string
+		Password     string
+		Database     string
+		TestDatabase string
+	}
+
+	// MailConfig stores the mail configuration
+	// MailConfig struct {
+	// 	Hostname    string
+	// 	Port        uint16
+	// 	User        string
+	// 	Password    string
+	// 	FromAddress string
+	// }
+)
+
+func GetConfig() (Config, error) {
+	var c Config
+	log.Println("[TRACE] GetConfig")
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("config")
+	viper.AddConfigPath("../config")
+	viper.AddConfigPath("../../config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Println("[FATAL] GetConfig: viper.ReadInConfig: ", err)
+		return c, err
+	}
+	if err := viper.Unmarshal(&c); err != nil {
+		log.Println("[FATAL] GetConfig: viper.Unmarshal: ", err)
+		return c, err
+	}
+	return c, nil
+}
